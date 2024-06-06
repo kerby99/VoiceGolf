5863500307fb94632e1f10a75acb3a824ee9b2fe
Kerby <kerby99.tineus@gmail.com>
2024-06-05 20:48:23 -0400

    The camera follow the ball now.

---
+++ Voice Golf/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
